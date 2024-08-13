50e08cd94e6c9de79565dc04541bbfd7fc449e15
MrHenrique <h_om_@hotmail.com>
2024-08-13 23:26:47 +0000

    First class 13/08/2024

---
+++ aula01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Models;
+
+public class Program{
+    public static void Main(){
+        Console.WriteLine("Cadastro de Pessoa");
+        Console.Write("Nome: ");
+        string nomePessoa = Console.ReadLine();
+        Console.Write("Idade: ");
+        int idadePessoa = int.Parse(Console.ReadLine());
+        Pessoa pessoaObj = new Pessoa(nomePessoa, idadePessoa);
+        Console.WriteLine(pessoaObj);
+    }
+}
