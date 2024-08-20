07637b6157c639a38d2ea516bf435c905f082aea
MrHenrique <h_om_@hotmail.com>
2024-08-20 23:28:50 +0000

    Aula 04

---
+++ aula04/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Pessoa objPessoa = new Pessoa("Henrique");
+        Console.WriteLine("Antes do método (Classe): " + objPessoa.ToString());
+        AlteraNomeClasse(objPessoa);
+        Console.WriteLine("Depois do método (Classe): " + objPessoa.ToString());
+
+        PessoaStruct objPessoaStruct = new PessoaStruct("HenriqueStruct");
+        Console.WriteLine("Antes do método (Struct): " + objPessoaStruct.ToString());
+        AlteraNomeStruct(objPessoaStruct);
+        Console.WriteLine("Depois do método (Struct): " + objPessoaStruct.ToString());
+    }
+    public static void AlteraNomeClasse(Pessoa objAux)
+    {
+
+        objAux.Nome = "Henrique Dois";
+    }
+    public static void AlteraNomeStruct(PessoaStruct objAux)
+    {
+        objAux.NomeStruct = "Henrique Dois";
+    }
+}
